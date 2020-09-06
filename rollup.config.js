70d87faf612f51b1503ba22d5f43be43ed4686e7
Cassidy Bandy <31383109+c-bandy@users.noreply.github.com>
2020-09-06 11:51:09 +0200

    consistent whitespace across the app

--- rollup.config.js
+++ rollup.config.js
@@ -16,90 +16,90 @@ const production = !process.env.ROLLUP_WATCH;
 const outputDir = 'build';
 
 function serve() {
-	let server;
+  let server;
 
-	function toExit() {
-		if (server) server.kill(0);
-	}
+  function toExit() {
+    if (server) server.kill(0);
+  }
 
-	return {
-		writeBundle() {
-			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
-			});
+  return {
+    writeBundle() {
+      if (server) return;
+      server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+        stdio: ['ignore', 'inherit', 'inherit'],
+        shell: true
+      });
 
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
+      process.on('SIGTERM', toExit);
+      process.on('exit', toExit);
+    }
+  };
 }
 
 function hashStaticAssets() {
-	return {
-		name: 'hash-static-assets',
-		writeBundle() {
-			posthtml([
-				// Hashes `bundle.[hash].css` and `bundle.[hash].js`
-				hash({ path: outputDir }),
-			])
-				.process(readFileSync('build/index.html'))
-				.then(result => writeFileSync('build/index.html', result.html));
-		},
-	};
+  return {
+    name: 'hash-static-assets',
+    writeBundle() {
+      posthtml([
+        // Hashes `bundle.[hash].css` and `bundle.[hash].js`
+        hash({ path: outputDir }),
+      ])
+        .process(readFileSync('build/index.html'))
+        .then(result => writeFileSync('build/index.html', result.html));
+    },
+  };
 }
 
 export default {
-	input: 'src/main.ts',
-	output: {
-		sourcemap: !production,
-		format: 'iife',
-		name: 'app',
-		file: join(outputDir, 'bundle.[hash].js'),
-	},
-	plugins: [
-		production && del({ targets: join(outputDir, '*') }),
-		copy({
-			targets: [{ src: 'public/*', dest: outputDir }]
-		}),
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css: css => {
-				css.write('bundle.[hash].css', !production);
-			},
-			preprocess: sveltePreprocess(),
-		}),
+  input: 'src/main.ts',
+  output: {
+    sourcemap: !production,
+    format: 'iife',
+    name: 'app',
+    file: join(outputDir, 'bundle.[hash].js'),
+  },
+  plugins: [
+    production && del({ targets: join(outputDir, '*') }),
+    copy({
+      targets: [{ src: 'public/*', dest: outputDir }]
+    }),
+    svelte({
+      // enable run-time checks when not in production
+      dev: !production,
+      // we'll extract any component CSS out into
+      // a separate file - better for performance
+      css: css => {
+        css.write('bundle.[hash].css', !production);
+      },
+      preprocess: sveltePreprocess(),
+    }),
 
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
-		resolve({
-			browser: true,
-			dedupe: ['svelte']
-		}),
-		commonjs(),
-		typescript({ sourceMap: !production }),
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration -
+    // consult the documentation for details:
+    // https://github.com/rollup/plugins/tree/master/packages/commonjs
+    resolve({
+      browser: true,
+      dedupe: ['svelte']
+    }),
+    commonjs(),
+    typescript({ sourceMap: !production }),
 
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
+    // In dev mode, call `npm run start` once
+    // the bundle has been generated
+    !production && serve(),
 
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload({ watch: outputDir }),
+    // Watch the `public` directory and refresh the
+    // browser on changes when not in production
+    !production && livereload({ watch: outputDir }),
 
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser(),
-		production && hashStaticAssets()
-	],
-	watch: {
-		clearScreen: false
-	}
+    // If we're building for production (npm run build
+    // instead of npm run dev), minify
+    production && terser(),
+    production && hashStaticAssets()
+  ],
+  watch: {
+    clearScreen: false
+  }
 };
