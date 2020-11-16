8720a3c133adbf4e47791a97afccc6990852566b
Cassidy Bandy <31383109+c-bandy@users.noreply.github.com>
2020-11-16 20:30:33 +0100

    style

--- src/Picker.svelte
+++ src/Picker.svelte
@@ -1,25 +1,24 @@
 <script lang="ts">
-  import { hiragana } from "@/lib/db";
+  import { hiragana, katakana } from "@/lib/db";
+  import { isHiragana } from "wanakana";
   import { getAnswers } from "./lib/answer";
 
   interface Kana {
     kana: string;
     checked: boolean;
+    type: string;
   }
 
   function createKanaRow(kana: string[]): Kana[] {
     return kana.map((item) => ({
       kana: item,
       checked: true,
+      type: isHiragana(item) ? "hiragana" : "katakana",
     }));
   }
 
   function isRowChecked(row: Kana[]): boolean {
     return row.every((item) => item.checked);
-  }
-
-  function isRowIndeterminate(row: Kana[]): boolean {
-    return row.some((item) => item.checked);
   }
 
   function checkRow(state: boolean) {
@@ -36,20 +35,29 @@
 </script>
 
 <style lang="scss">
+  $grid-margin: 8px;
+
   input[type="checkbox"] {
     transform: scale(1.5);
     margin: 0 1.5em;
+    cursor: pointer;
   }
 
-  $grid-margin: 4px;
+  label {
+    cursor: pointer;
+  }
+
+  .check-column {
+    margin-top: 1em;
+    margin-bottom: 1em;
+  }
 
   .row {
     display: flex;
     align-items: center;
-    margin-top: $grid-margin;
 
-    &:first-of-type {
-      margin-top: 0;
+    &:not(:first-of-type) {
+      margin-top: $grid-margin;
     }
   }
   .row-items {
@@ -59,62 +67,77 @@
     font-size: 2em;
     display: flex;
     color: var(--text-color-light);
-    background: none;
-    border: 2px solid var(--background-contrast-light);
+    background: transparent;
+    border: 3px solid var(--text-color-lighter);
     border-radius: var(--standard-border-radius);
+    outline: none;
     margin-left: $grid-margin;
     user-select: none;
+    cursor: pointer;
     line-height: 1;
-    padding: 0.2em;
-    transition: 125ms var(--standard-transition) color,
-      125ms var(--standard-transition) background,
-      125ms var(--standard-transition) border-color;
+    padding: 7px;
+    transition: 150ms var(--standard-transition) color,
+      150ms var(--standard-transition) background,
+      150ms var(--standard-transition) border-color;
+
+    &:first-of-type {
+      margin-left: 0;
+    }
+
+    &:hover,
+    &:focus {
+      color: var(--text-color);
+      border-color: var(--text-color-light);
+    }
 
     &.checked {
       color: var(--text-color-on-accent-color);
       background: var(--accent-color);
       border-color: transparent;
-    }
 
-    &:first-of-type {
-      margin-left: 0;
+      &:hover,
+      &:focus {
+        border-color: var(--border-color-on-accent-color);
+      }
     }
   }
 </style>
 
 <div class="picker-grid">
-  <input
-    type="checkbox"
-    title="Check entire column"
-    checked={rows.every(isRowChecked)}
-    indeterminate={rows.some(isRowIndeterminate) && !rows.every(isRowChecked)}
-    on:click={() => {
-      const everyRowChecked = rows.every(isRowChecked);
-      rows = rows.map(checkRow(!everyRowChecked));
-    }} />
+  <div class="row check-column">
+    <input
+      type="checkbox"
+      id="check-column"
+      checked={rows.every(isRowChecked)}
+      on:click={() => {
+        const everyRowChecked = rows.every(isRowChecked);
+        rows = rows.map(checkRow(!everyRowChecked));
+      }} />
+    <label for="check-column">Entire column</label>
+  </div>
   {#each rows as row}
     <div class="row">
       <input
         type="checkbox"
         title={isRowChecked(row) ? 'Uncheck entire row' : 'Check entire row'}
         checked={isRowChecked(row)}
-        indeterminate={isRowIndeterminate(row) && !isRowChecked(row)}
+        indeterminate={!isRowChecked(row) && row.some((item) => item.checked)}
         on:click={() => {
           const checked = isRowChecked(row);
           row = checkRow(!checked)(row);
         }} />
       <div class="row-items">
-        {#each row as rowItem}
-          <button
+        {#each row as item}
+          <div
             class="item"
-            class:checked={rowItem.checked}
-            title={getAnswers(rowItem.kana)[0]}
-            aria-checked={rowItem.checked}
+            class:checked={item.checked}
+            title={item.type + ' ' + getAnswers(item.kana)[0]}
             on:click={() => {
-              rowItem.checked = !rowItem.checked;
+              item.checked = !item.checked;
             }}>
-            {rowItem.kana}
-          </button>
+            <input type="checkbox" style="display: none" />
+            {item.kana}
+          </div>
         {/each}
       </div>
     </div>
