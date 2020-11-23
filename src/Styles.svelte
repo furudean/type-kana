f000e19a3bdf6cddc5516af19cf42a1b8dea3238
Cassidy Bandy <31383109+c-bandy@users.noreply.github.com>
2020-11-23 19:07:17 +0100

    fix style for #d0f1e70

--- src/Styles.svelte
+++ src/Styles.svelte
@@ -18,6 +18,8 @@
       height: 1em;
       background-color: var(--accent-color);
       mask-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24'%3E%3Cpath d='M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z' fill='currentColor'%3E%3C/path%3E%3C/svg%3E");
+      mask-repeat: no-repeat;
+      mask-position: 50% 50%;
     }
   }
 </style>
