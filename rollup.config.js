25632d3c8eb03f5dbcd3a9c90f384349df4453ce
Cassidy Bandy <31383109+c-bandy@users.noreply.github.com>
2020-12-07 18:34:28 +0100

    use rollup-plugin-typescript2

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import del from 'rollup-plugin-delete'
 import { join } from 'path';
 import posthtml from 'posthtml';
