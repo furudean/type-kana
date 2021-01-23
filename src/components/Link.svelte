cb9def809f6a0efc56c9c94c4f93ebc7ae160149
Cassidy Bandy <31383109+c-bandy@users.noreply.github.com>
2021-01-23 13:01:38 -0700

    smarter way of setting :after element data on links

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+  import { mdiOpenInNew } from '@mdi/js';
+  import { svgToDataUrl } from '@/lib/util';
+
   export let href: string;
   export let target = "_self";
   export let rel = "noopener";
+
+  let element: HTMLElement;
+
+  onMount(() => {
+    const iconDataUrl = svgToDataUrl(mdiOpenInNew);
+    element.style.setProperty('--icon-url', `url("${iconDataUrl}")`);
+  });
 </script>
 
 <style lang="scss">
@@ -29,7 +40,7 @@
       width: 1em;
       height: 1em;
       background-color: var(--accent-color);
-      mask-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z' %3E%3C/path%3E%3C/svg%3E");
+      mask-image: var(--icon-url);
       mask-repeat: no-repeat;
       mask-position: 50% 50%;
     }
@@ -39,6 +50,6 @@
   }
 </style>
 
-<a {href} {target} {rel} {...$$restProps}>
+<a {href} {target} {rel} {...$$restProps} bind:this={element}>
   <slot />
 </a>
--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,3 +1,5 @@
+import { oneLine } from 'common-tags';
+
 export async function sleep(ms: number): Promise<void> {
   return new Promise(resolve => { setTimeout(resolve, ms) });
 }
@@ -21,4 +23,14 @@ export function throttle<F extends Function>(
       }, ms);
     }
   } as any; // Cast needed for compiler to not freak out
+}
+
+export function svgToDataUrl(path: string, viewBox = "0 0 24 24") {
+  const svg = oneLine`
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="${viewBox}">
+      <path d="${path}"></path>
+    </svg>
+  `;
+
+  return "data:image/svg+xml," + encodeURI(svg);
 }
