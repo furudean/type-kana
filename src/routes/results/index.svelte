8623369aa469ccc542301578d026487604fe0808
Cassidy Bandy <31383109+c-bandy@users.noreply.github.com>
2021-02-25 22:49:42 -0700

    dotdot

--- src/routes/results/index.svelte
+++ src/routes/results/index.svelte
@@ -8,6 +8,13 @@
 
 <div class="container">
   <h1>Session summary</h1>
+  <section class="summary-box">
+    {#each $summary.incorrect as item}
+      <div class="summary-item">
+        {item.kana}
+      </div>
+    {/each}
+  </section>
   <pre>
     {JSON.stringify($summary, null, 2)}
   </pre>
@@ -15,15 +22,17 @@
 
 <style lang="scss">
   .container {
-    height: 100%;
     background-color: var(--accent-color);
     color: var(--text-color-on-accent-color);
-    overflow: hidden;
     padding: 0 2em;
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
+    overflow: auto;
+  }
+
+  .summary-box {
+    background-color: var(--background-color);
+    color: var(--text-color);
+  }
+
+  .summary-item {
   }
 </style>
