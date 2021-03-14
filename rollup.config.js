9afda264c5581e77979df95ff1e34926aba847e5
Cassidy Bandy <31383109+c-bandy@users.noreply.github.com>
2021-03-13 22:26:29 -0700

    try target

--- rollup.config.js
+++ rollup.config.js
@@ -80,8 +80,7 @@ export default {
           "@babel/plugin-syntax-dynamic-import",
           ["@babel/plugin-transform-runtime", {
             useESModules: true
-          }],
-          "@babel/plugin-proposal-optional-chaining"
+          }]
         ]
       }),
 
