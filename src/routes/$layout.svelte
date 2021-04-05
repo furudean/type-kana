602e1de9ede78695ec395efce9953da70a5169fa
Cassidy Bandy <31383109+c-bandy@users.noreply.github.com>
2021-04-04 20:19:00 -0600

    define global css as files instead of using global

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -2,9 +2,9 @@
 	import { onMount } from "svelte"
 	import { resolvedTheme } from "@/stores/theme"
 	import { getAudioContext, getRootGain } from "@/lib/audio"
-	import Theme from "@/styles/Theme.svelte"
-	import Global from "@/styles/Global.svelte"
 	import { settings } from "@/stores/settings"
+	import "@/styles/theme.css"
+	import "@/styles/global.scss"
 
 	onMount(() => {
 		resolvedTheme.subscribe((theme) => {
@@ -32,8 +32,6 @@
 	}}
 />
 
-<Global />
-<Theme />
 <main>
 	<slot />
 </main>
