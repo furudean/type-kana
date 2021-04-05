4f4cc8e0422afe3d170c16c20753cba1eb8c9bb0
Cassidy Bandy <31383109+c-bandy@users.noreply.github.com>
2021-04-05 16:52:19 -0600

    no confetti for those that do nothing :-(

--- src/routes/summary/index.svelte
+++ src/routes/summary/index.svelte
@@ -23,26 +23,28 @@
 	}
 
 	onMount(() => {
-		fire(0.25, {
-			spread: 26,
-			startVelocity: 55
-		})
-		fire(0.35, {
-			spread: 100,
-			decay: 0.91
-		})
-		fire(0.2, {
-			spread: 60
-		})
-		fire(0.1, {
-			spread: 120,
-			startVelocity: 25,
-			decay: 0.92
-		})
-		fire(0.1, {
-			spread: 120,
-			startVelocity: 45
-		})
+		if (answered > 0) {
+			fire(0.25, {
+				spread: 26,
+				startVelocity: 55
+			})
+			fire(0.35, {
+				spread: 100,
+				decay: 0.91
+			})
+			fire(0.2, {
+				spread: 60
+			})
+			fire(0.1, {
+				spread: 120,
+				startVelocity: 25,
+				decay: 0.92
+			})
+			fire(0.1, {
+				spread: 120,
+				startVelocity: 45
+			})
+		}
 	})
 </script>
 
