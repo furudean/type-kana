0884195bb3036b0bcd18a04ab078a64f305b927a
Cassidy Bandy <31383109+c-bandy@users.noreply.github.com>
2021-04-04 20:25:26 -0600

    uniform particles

--- src/routes/summary/index.svelte
+++ src/routes/summary/index.svelte
@@ -29,8 +29,7 @@
 		})
 		fire(0.35, {
 			spread: 100,
-			decay: 0.91,
-			scalar: 0.8
+			decay: 0.91
 		})
 		fire(0.2, {
 			spread: 60
@@ -38,8 +37,7 @@
 		fire(0.1, {
 			spread: 120,
 			startVelocity: 25,
-			decay: 0.92,
-			scalar: 1.2
+			decay: 0.92
 		})
 		fire(0.1, {
 			spread: 120,
