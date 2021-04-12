2433aed0eda3fdef17bc07411f8a4ddb15a01f43
Cassidy Bandy <31383109+c-bandy@users.noreply.github.com>
2021-04-11 21:03:51 -0600

    less complicated confetti

--- src/routes/summary/confetti.ts
+++ src/routes/summary/confetti.ts
@@ -1,38 +1,13 @@
 import confetti from "canvas-confetti"
-import type { Options } from "canvas-confetti"
-
-const particles = 150
-
-function emit(particleRatio: number, opts: Options) {
-	confetti({
-		disableForReducedMotion: true,
-		origin: { y: 0.4 },
-		gravity: 1.3,
-		ticks: 400,
-		...opts,
-		particleCount: Math.floor(particles * particleRatio)
-	})
-}
 
 export function celebrate() {
-	emit(0.25, {
-		spread: 26,
-		startVelocity: 50
-	})
-	emit(0.35, {
-		spread: 100,
-		decay: 0.91
-	})
-	emit(0.2, {
-		spread: 80
-	})
-	emit(0.1, {
-		spread: 120,
-		startVelocity: 30,
-		decay: 0.92
-	})
-	emit(0.1, {
-		spread: 120,
-		startVelocity: 60
+	confetti({
+		disableForReducedMotion: true,
+		origin: { y: 0.6 },
+		gravity: 1.4,
+		spread: 50,
+		ticks: 400,
+		particleCount: 90,
+		startVelocity: 80
 	})
 }
