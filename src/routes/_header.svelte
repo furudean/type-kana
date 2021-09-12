464357d4a510a1328b5df2add3cab9141f42bf07
Cassidy Bandy <31383109+c-bandy@users.noreply.github.com>
2021-09-12 13:55:28 -0600

    headers goes in own file, minor tweaks

---
+++ src/routes/_header.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+	import Logo from "$lib/Logo.svelte"
+	import Icon from "@/lib/Icon.svelte"
+	import { mdiGithub, mdiOpenInNew } from "@mdi/js"
+</script>
+
+<section class="header content-padding">
+	<div class="content-width center">
+		<h1><Logo size="2em" /> Type Kana</h1>
+		<ul>
+			<li>
+				<a
+					href="https://github.com/c-bandy/type-kana"
+					target="_blank"
+					rel="nofollow"
+					class="menu-link"
+				>
+					<Icon size="1.25em" path={mdiGithub} />
+					source
+					<Icon size="1em" path={mdiOpenInNew} />
+				</a>
+			</li>
+			<li>
+				<a
+					href="https://cass.moe"
+					target="_blank"
+					rel="nofollow"
+					class="menu-link"
+				>
+					about
+					<Icon size="1em" path={mdiOpenInNew} />
+				</a>
+			</li>
+		</ul>
+	</div>
+</section>
+
+<style lang="postcss">
+	.header {
+		background: var(--background-contrast);
+
+		> div {
+			display: flex;
+			flex-direction: row;
+			justify-content: space-between;
+		}
+
+		> div > :last-child {
+			margin-bottom: 0;
+		}
+	}
+
+	h1 {
+		margin: 0;
+		display: flex;
+		align-items: center;
+		gap: 0.5em;
+	}
+
+	ul {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: flex-end;
+		padding: 0;
+		margin-top: 0;
+		gap: calc(0.5 * var(--line-space));
+	}
+
+	li {
+		list-style: none;
+
+		a {
+			text-decoration: none;
+			display: flex;
+			gap: 0.5em;
+			border-bottom: 1px solid transparent;
+			align-items: center;
+			line-height: 1;
+
+			&:hover {
+				border-color: currentColor;
+			}
+		}
+	}
+
+	.menu-link {
+		color: var(--text-color);
+	}
+
+	.text-link:before {
+		background-color: currentColor !important;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,48 +1,22 @@
 <script lang="ts">
-	import Logo from "$lib/Logo.svelte"
+	import Header from "./_header.svelte"
 	import Button from "$lib/Button.svelte"
 	import Icon from "@/lib/Icon.svelte"
-	import { mdiArrowRight, mdiGithub } from "@mdi/js"
+	import { mdiArrowRight } from "@mdi/js"
 </script>
 
 <svelte:head>
 	<title>Type Kana</title>
 </svelte:head>
 
-<section class="header content-padding">
-	<div class="content-width center">
-		<h1><Logo size="2em" /> Type Kana</h1>
-		<p>Proxy, warez. Anything's possible.</p>
-		<ul>
-			<li>
-				<a
-					class="menu-link"
-					href="https://github.com/c-bandy/type-kana"
-					target="_blank"
-					rel="nofollow"
-				>
-					<Icon size="1.5em" path={mdiGithub} />
-					<span>Source</span>
-				</a>
-			</li>
-		</ul>
-	</div>
-</section>
+<Header />
 <section class="content-padding">
 	<div class="content-width center">
 		<p>
 			Type Kana is a quiz app to help you learn hiragana and katakana, the
 			Japanese syllabaries.
 		</p>
-		<p>
-			This was made by
-			<a
-				href="https://cass.moe"
-				target="_blank"
-				rel="nofollow"
-				class="text-link">Cassidy Bandy</a
-			>.
-		</p>
+
 		<Button href="/setup">
 			Let's begin
 			<Icon path={mdiArrowRight} size="1.5em" />
@@ -51,45 +25,6 @@
 </section>
 
 <style lang="postcss">
-	.header {
-		background: var(--background-contrast);
-
-		div > :last-child {
-			margin-bottom: 0;
-		}
-
-		h1 {
-			margin-bottom: calc(0.5 * var(--line-space));
-			display: flex;
-			align-items: center;
-			gap: 0.5em;
-		}
-
-		ul {
-			display: flex;
-			padding: 0;
-		}
-
-		li {
-			list-style: none;
-
-			a {
-				text-decoration: none;
-				display: flex;
-				gap: 0.5em;
-				border-bottom: 1px solid transparent;
-
-				&:hover {
-					border-color: currentColor;
-				}
-			}
-		}
-	}
-
-	.menu-link {
-		color: var(--text-color);
-	}
-
 	* > :global(.button .svg-icon) {
 		margin-right: -0.5em;
 	}
