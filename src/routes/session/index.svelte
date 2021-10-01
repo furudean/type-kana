116c794a0f8898e2ff251f169c622ba4f1490ffb
Cassidy Bandy <31383109+c-bandy@users.noreply.github.com>
2021-09-30 20:37:34 -0600

    go to summary faster

--- src/routes/session/index.svelte
+++ src/routes/session/index.svelte
@@ -99,7 +99,7 @@
 	})
 
 	// go to results if queue is empty
-	$: unquizzed.length === 0 && setTimeout(() => goto("summary"), 1000)
+	$: unquizzed.length === 0 && setTimeout(() => goto("summary"), 500)
 </script>
 
 <svelte:head>
