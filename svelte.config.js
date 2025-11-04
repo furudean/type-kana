b0533e42a41f614e40a23fcd6fccf6cecc051067
Jose Quijada <quijadajosed@gmail.com>
2025-11-04 16:55:40 -0300

    Fix: Resolve svelte-check errors with missing configuration

---
+++ svelte.config.js
@@ -0,0 +1,14 @@
+import adapter from "@sveltejs/adapter-vercel"
+import preprocess from "svelte-preprocess"
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: preprocess({
+		postcss: true
+	}),
+	kit: {
+		adapter: adapter()
+	}
+}
+
+export default config
