4c6c019a06167c15afd06d1d8d608dcf7bc90be0
Jose Quijada <quijadajosed@gmail.com>
2025-11-12 15:54:53 -0300

    rollback svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,54 @@
-import adapter from "@sveltejs/adapter-vercel"
-import preprocess from "svelte-preprocess"
+import sveltePreprocess from "svelte-preprocess"
+import adapterVercel from "@sveltejs/adapter-vercel"
+import path from "path"
+import { readdirSync } from "fs"
+
+/**
+ * Lists urls of all static routes in `path`.
+ *
+ * @param {string} p - The path to search
+ *
+ * @returns {string[]}
+ */
+function listRoutesIn(p) {
+	const routesPath = path.join("./src/routes/", p)
+	const dir = readdirSync(routesPath, { withFileTypes: true })
+
+	const filenames = dir
+		.filter(({ isFile, name }) => isFile && !name.startsWith("_"))
+		.map((dirent) => dirent.name)
+
+	if (filenames.length === 0) {
+		throw new Error(`listRoutesIn: No static routes found in '${p}'`)
+	}
+
+	const routes = filenames.map((filename) => {
+		const name = path.parse(filename).name
+		return path.posix.join("/", p, name)
+	})
+
+	return routes
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess({
-		postcss: true
-	}),
+	// an array of file extensions that should be treated as Svelte components
+	extensions: [".svelte"],
+
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapterVercel(),
+		prerender: {
+			enabled: true,
+			default: true,
+			entries: ["*", ...listRoutesIn("/icon/")]
+		},
+		alias: {
+			$: "src"
+		}
+	},
+
+	// options passed to svelte.preprocess (https://svelte.dev/docs#svelte_preprocess)
+	preprocess: sveltePreprocess({ postcss: true, typescript: true })
 }
 
 export default config
